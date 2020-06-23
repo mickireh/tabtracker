a6a4549f53decb82240785dda42dcae313ea62ae
Micki <gsmhirsch@gmail.com>
2020-06-23 15:52:11 +0200

    client to server req working, res.body undefined (body-parser problem)

---
+++ client/src/components/Register.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email" />
+    <input
+      type="password"
+      name="password"
+      placeholder="password"
+      v-model="password"
+    />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "../services/AuthenticationService";
+export default {
+  name: "register",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      // console.log('clicked', this.email, this.password);
+      console.log(response.data)
+    }
+  }
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed', value)
+  //   }
+  // },
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.email = "hello world"
+  //   }, 2000)
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,18 @@
 console.log('hello');
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express();
 app.use(morgan('combined'))
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'Hi World'
+        message: `Hello, User was registered`
+        // message: `Hello ${res.body.email} User was registered`
     })
 })
 
