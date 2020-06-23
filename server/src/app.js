60812f02ad30906feebed1b32217b248080e62b4
Micki <gsmhirsch@gmail.com>
2020-06-23 17:52:15 +0200

    req.body, not res.body

--- server/src/app.js
+++ server/src/app.js
@@ -6,8 +6,6 @@ const morgan = require('morgan');
 // const bodyParser = require('body-parser');
 
 const app = express();
-app.use(morgan('combined'))
-app.use(cors())
 
 // with express 4.16 (this is 4.17) body-parser is again included in express, but both should work
 app.use(express.json())
@@ -15,11 +13,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(morgan('combined'))
+app.use(cors())
+
 
 app.post('/register', (req, res) => {
+
     res.send({
-        message: `Hello, User was registered`
-        // message: `Hello ${res.body.email} User was registered`
+        // message: `Hello, User was registered`
+        message: `Hello ${req.body.email} User was registered`
     })
 })
 
