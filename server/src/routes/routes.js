177bde4a2e30158b662e16c4d2416aa8613ff783
Micki <gsmhirsch@gmail.com>
2020-06-23 21:45:27 +0200

    register user into db

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,8 +1,13 @@
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      // message: `Hello, User was registered`
-      message: `Hello ${req.body.email}, User was registered`
-    })
-  })
+  const users = require("../controllers/user.controller.js");
+
+  // app.post('/register', (req, res) => {
+  //   res.send({
+  //     // message: `Hello, User was registered`
+  //     message: `Hello ${req.body.email}, User was registered`
+  //   })
+  // })
+
+  app.post('/register', users.create)
+  
 }
