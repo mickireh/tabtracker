cc422834da30ef364ed1fecb99c91a8a042735ec
Thomas Hirsch <thokari@users.noreply.github.com>
2020-06-24 21:17:38 +0200

    Update AuthenticationController.js

--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -14,9 +14,7 @@ module.exports = {
             const user = await User.create(req.body)
             res.send(user.toJSON())
         } catch (err) {
-            res.status(400).send({
-                error: 'This email account is already in use.'
-            })
+            res.status(400).send(JSON.stringify(err))
         }
     }
 
